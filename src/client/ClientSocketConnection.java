d0eca026fd6bd925aeaa026044e143b88a641a39
CheesusChrist <32204044+CheesusChrist@users.noreply.github.com>
2019-11-20 16:08:12 -0700

    added socketcontroller classes

--- src/client/ClientSocketConnection.java
+++ src/client/ClientSocketConnection.java
@@ -4,9 +4,8 @@ import server.SocketConnection;
 import java.net.Socket;
 
 public class ClientSocketConnection extends SocketConnection {
-    public ClientSocketConnection(Socket clientSocket) {
-        super(clientSocket);
+    public ClientSocketConnection(Socket serverSocket) {
+        super(serverSocket);
     }
 
-    
 }
