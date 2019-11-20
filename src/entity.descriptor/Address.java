d3e0efb005c5f796e181d896d9052d698fe499f0
JedediahH <38731180+JedediahH@users.noreply.github.com>
2019-11-19 23:31:11 -0700

    Added packages for different entities, and added classes

---
+++ src/entity.descriptor/Address.java
@@ -0,0 +1,20 @@
+package entity.descriptor;
+
+public class Address
+{
+	private int streetNumber;
+	private String street;
+	private String city;
+	private String province;
+	private String postalCode;
+
+	public Address( int streetNumber, String street, String city, String province, String postalCode )
+	{
+		this.streetNumber = streetNumber;
+		this.street = street;
+		this.city = city;
+		this.province = province;
+		this.postalCode = postalCode;
+	}
+
+}
---
+++ src/entity.socket/Property.java
@@ -0,0 +1,45 @@
+package entity.socket;
+
+enum PropertyStatus
+{
+	AVAILABLE,
+	SUSPENDED,
+	RENTED,
+	REMOVED
+}
+
+enum Quadrant
+{
+	NW,
+	NE,
+	SW,
+	SE
+}
+
+enum PropertyType
+{
+	HOUSE,
+	DUPLEX,
+	TOWNHOUSE,
+	APARTMENT,
+	CONDO,
+	MAINFLOOR,
+	BASEMENT
+}
+
+public class Property
+{
+	private Address address;
+	private Quadrant quadrant;
+	private PropertyStatus status;
+	private PropertyTraits traits;
+
+	public Property( Address address, Quadrant quadrant, PropertyStatus status, PropertyTraits traits )
+	{
+		this.address = address;
+		this.quadrant = quadrant;
+		this.status = status;
+		this.traits = traits;
+	}
+
+}
