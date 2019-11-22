76b38533325e59dfa0d1f76ed058609188a26128
JedediahH <38731180+JedediahH@users.noreply.github.com>
2019-11-21 17:38:35 -0700

    Corrected package folders and added serialization to Property

---
+++ src/entity/socket/property/Property.java
@@ -0,0 +1,113 @@
+package entity.socket.property;
+
+import descriptor.Address;
+import java.io.Serializable;
+import java.io.*;
+
+public class Property implements Serializable
+{
+	private Address address;
+	private Quadrant quadrant;
+	private PropertyStatus status;
+	private PropertyTraits traits;
+	private int id;
+
+	private static final long serialVersionUID = 3L;
+
+	static private int count = 0;
+
+	public Property( Address address, Quadrant quadrant, PropertyStatus status, PropertyTraits traits )
+	{
+		this.address = address;
+		this.quadrant = quadrant;
+		this.status = status;
+		this.traits = traits;
+		
+		this.id = 1000 + count;
+		count++;
+	}
+
+    public Address getAddress()
+    {
+        return address;
+    }
+    public Quadrant getQuadrant()
+    {
+        return quadrant;
+    }
+    public PropertyStatus getStatus()
+    {
+        return status;
+    }
+    public PropertyTraits getTraits()
+    {
+        return traits;
+    }
+    public int getId()
+    {
+        return id;
+    }
+
+	public static void main(String[] args) 
+    {   
+    	PropertyTraits pt = new PropertyTraits( PropertyType.HOUSE, 1, 1, 1000, true );
+        Address ad = new Address( 3307, "24 Street NW", "Calgary", "AB", "T2M3Z8" );
+        Property object = new Property( ad, Quadrant.NW, PropertyStatus.AVAILABLE, pt );
+        String filename = "file.ser";
+          
+        // Serialization
+        try
+        {    
+            //Saving of object in a file 
+            FileOutputStream file = new FileOutputStream(filename); 
+            ObjectOutputStream out = new ObjectOutputStream(file); 
+              
+            // Method for serialization of object 
+            out.writeObject(object); 
+              
+            out.close(); 
+            file.close(); 
+              
+            System.out.println("Object has been serialized"); 
+  
+        } 
+          
+        catch(IOException ex) 
+        { 
+            System.out.println("IOException is caught"); 
+        } 
+  
+  
+        Property object1 = null; 
+  
+        // Deserialization 
+        try
+        {    
+            // Reading the object from a file 
+            FileInputStream file = new FileInputStream(filename); 
+            ObjectInputStream in = new ObjectInputStream(file); 
+              
+            // Method for deserialization of object 
+            object1 = (Property)in.readObject(); 
+              
+            in.close(); 
+            file.close(); 
+              
+            System.out.println("Object has been deserialized ");
+            System.out.println("id = " + object1.getId() );
+            System.out.println("Quadrant = " + object1.getQuadrant() );
+        }
+          
+        catch(IOException ex) 
+        { 
+            System.out.println("IOException is caught"); 
+        } 
+          
+        catch(ClassNotFoundException ex) 
+        { 
+            System.out.println("ClassNotFoundException is caught"); 
+        } 
+  
+    } 
+
+}
