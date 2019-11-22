39de12a3143fa267441aff13e107dd91d4480fe3
CheesusChrist <32204044+CheesusChrist@users.noreply.github.com>
2019-11-22 01:51:48 -0700

    tried to remove db file from repo

--- src/server/DatabaseHelper.java
+++ src/server/DatabaseHelper.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class DatabaseController {
+public class DatabaseHelper {
 
 
     public static void main(String[] args) throws SQLException {
