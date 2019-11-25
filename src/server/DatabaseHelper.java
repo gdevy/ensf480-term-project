3a4d3f8d31e93444acd0d8373a6cdad6d2e61bb5
CheesusChrist <32204044+CheesusChrist@users.noreply.github.com>
2019-11-25 12:31:07 -0700

    fixed register property with correct PreparedStatement. started searchSavedSearches()

--- src/server/DatabaseHelper.java
+++ src/server/DatabaseHelper.java
@@ -5,7 +5,9 @@ import descriptor.LoginInfo;
 import descriptor.UserTypeLogin;
 import entity.socket.PropertySearchCriteria;
 import entity.socket.property.*;
+import org.sqlite.SQLiteDataSource;
 
+import javax.naming.directory.SearchControls;
 import java.io.*;
 import java.sql.*;
 import java.util.*;
@@ -26,17 +28,17 @@ public class DatabaseHelper {
         Property object = new Property(1000, ad, Quadrant.NW, PropertyStatus.AVAILABLE, pt);
 
         PropertySearchCriteria psc = new PropertySearchCriteria();
-        psc.setMaxMonthlyRent( 2000 );
-        psc.setMinBathrooms( 2 );
-        psc.addQuadrant( Quadrant.NW );
-        psc.addQuadrant( Quadrant.SW );
-        psc.addType( PropertyType.HOUSE );
-        psc.addType( PropertyType.APARTMENT );
-        psc.setFurnished( true );
+        psc.setMaxMonthlyRent(2000);
+        psc.setMinBathrooms(2);
+        psc.addQuadrant(Quadrant.NW);
+        psc.addQuadrant(Quadrant.SW);
+        psc.addType(PropertyType.HOUSE);
+        psc.addType(PropertyType.APARTMENT);
+        psc.setFurnished(true);
 
         DatabaseHelper dbHelper = new DatabaseHelper();
 
-        //dbHelper.registerProperty(object);
+        dbHelper.registerProperty(object);
         //dbHelper.searchProperty(psc);
         LoginInfo info = new LoginInfo("greg", "abc123");
         System.out.println(dbHelper.attemptLogin(info));
@@ -44,44 +46,23 @@ public class DatabaseHelper {
     }
 
     boolean registerProperty(Property property) throws SQLException {
-        StringBuilder sb = new StringBuilder();
-        Statement stm = dbConnection.createStatement();
-        ResultSet rs = null;
-        rs = stm.executeQuery("SELECT quadrant_id\n" +
-                "from quadrant\n" +
-                "WHERE quadrant = '" + property.getQuadrant().name() + "';");
 
-        int quadrantID = rs.getInt("quadrant_id");
+        PreparedStatement statement = dbConnection.prepareStatement("INSERT INTO properties (property_id, quadrant, property_status, property_type, bathrooms, bedrooms, furnished, square_footage, monthly_rent, streetNumber, street, city, province, postal_code) values" +
+                " (null, (SELECT quadrant_id from quadrant WHERE quadrant = ?), (SELECT status_id from property_status WHERE status = ?), (SELECT type_id from property_type WHERE type =?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-        rs = stm.executeQuery("SELECT status_id\n" +
-                "from property_status\n" +
-                "WHERE status = '" + property.getStatus().name() + "';\n");
-
-        int propertyStatusID = rs.getInt("status_id");
-
-        rs = stm.executeQuery("SELECT type_id\n" +
-                "from property_type\n" +
-                "WHERE type = '" + property.getTraits().getType().name() + "';");
-
-
-        int propertyTypeID = rs.getInt("type_id");
-
-
-        PreparedStatement statement = dbConnection.prepareStatement("INSERT INTO properties (property_id, quadrant, property_status, property_type, bathrooms, bedrooms, furnished, square_footage, monthly_rent, streetNumber, street, city, province, postal_code) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        statement.setInt(1, 4);
-        statement.setInt(2, quadrantID);
-        statement.setInt(3, propertyStatusID);
-        statement.setInt(4, propertyTypeID);
-        statement.setInt(5, property.getTraits().getBathrooms());
-        statement.setInt(6, property.getTraits().getBedrooms());
-        statement.setBoolean(7, property.getTraits().getFurnished());
-        statement.setDouble(8, property.getTraits().getSquareFootage());
-        statement.setInt(9, property.getMonthlyRent());
-        statement.setInt(10, property.getAddress().getStreetNumber());
-        statement.setString(11, property.getAddress().getStreet());
-        statement.setString(12, property.getAddress().getCity());
-        statement.setString(13, property.getAddress().getProvince());
-        statement.setString(14, property.getAddress().getPostalCode());
+        statement.setString(1, property.getQuadrant().name());
+        statement.setString(2, property.getStatus().name());
+        statement.setString(3, property.getTraits().getType().name());
+        statement.setInt(4, property.getTraits().getBathrooms());
+        statement.setInt(5, property.getTraits().getBedrooms());
+        statement.setBoolean(6, property.getTraits().getFurnished());
+        statement.setDouble(7, property.getTraits().getSquareFootage());
+        statement.setInt(8, property.getMonthlyRent());
+        statement.setInt(9, property.getAddress().getStreetNumber());
+        statement.setString(10, property.getAddress().getStreet());
+        statement.setString(11, property.getAddress().getCity());
+        statement.setString(12, property.getAddress().getProvince());
+        statement.setString(13, property.getAddress().getPostalCode());
         System.out.println(statement);
         return statement.executeUpdate() == 1;  //double check this. javaDocs is a little ambiguous on this
     }
@@ -207,6 +188,91 @@ public class DatabaseHelper {
         }
 
         return results;
+    }
+
+    ArrayList<PropertySearchCriteria> searchSavedSearches(String userName) throws SQLException {
+        ArrayList<PropertySearchCriteria> results = new ArrayList<>();
+        Statement stm = dbConnection.createStatement();
+        ResultSet rs = stm.executeQuery("SELECT user_id FROM users\n" +
+                "WHERE email = '" + userName + "'");
+
+        int userID = rs.getInt("user_id");
+
+        rs = stm.executeQuery("SELECT * from saved_search_criteria\n" +
+                "WHERE user_id = " + userID + "");
+
+        while (rs.next()) {
+
+        }
+        return results;
+    }
+
+    void saveSearchCriteria(PropertySearchCriteria psc, String userInfo) throws SQLException {
+        Statement stm = dbConnection.createStatement();
+        ResultSet rs = stm.executeQuery("SELECT user_id FROM users\n" +
+                "WHERE email = '" + userInfo + "'");
+
+        int userID = rs.getInt("user_id");
+
+        rs = stm.executeQuery("SELECT * from saved_search_criteria\n" +
+                "WHERE user_id = " + userID + "");
+
+        PreparedStatement statement = dbConnection.prepareStatement("INSERT INTO saved_search_criteria (search_id, user_id, max_monthly_rent, min_bathrooms, min_bedrooms, min_square_footage) values (null, ?, ?, ?, ?, ?)");
+        statement.setInt(1, userID);
+        if (psc.hasMaxMonthlyRent()) {
+            statement.setInt(2, psc.getMaxMonthlyRent());
+        } else {
+            statement.setNull(2, Types.INTEGER);
+        }
+        if (psc.hasMinBathrooms()) {
+            statement.setInt(3, psc.getMinBathrooms());
+        } else {
+            statement.setNull(3, Types.INTEGER);
+        }
+        if (psc.hasMinBedrooms()) {
+            statement.setInt(4, psc.getMinBedrooms());
+        } else {
+            statement.setNull(4, Types.INTEGER);
+        }
+        if (psc.hasMinSquareFootage()) {
+            statement.setInt(5, psc.getMinSquareFootage());
+        } else {
+            statement.setNull(5, Types.INTEGER);
+        }
+
+        rs = stm.executeQuery("SELECT last_insert_rowid()");
+
+        int searchID = rs.getInt("last_insert_rowid()");
+
+        StringBuilder insertUpdate;
+        boolean firstRow;
+        if (psc.hasType()) {
+            firstRow = true;
+            insertUpdate = new StringBuilder("INSERT INTO search_property_type (search_id, property_type_id)\n" +
+                    "VALUES\n");
+            for (PropertyType type : psc.getTypes()) {
+                int propertyTypeID = 1;
+                if (!firstRow) {
+                    insertUpdate.append(",\n");
+                }
+                insertUpdate.append("(" + searchID + ", (SELECT type_id from property_type WHERE type = '" + type.name() + "'))");
+                firstRow = false;
+            }
+        }
+        if (psc.hasQuadrant()) {
+            firstRow = true;
+            insertUpdate = new StringBuilder("INSERT INTO search_quadrant (search_id, quadrant_id)\n" +
+                    "VALUES\n");
+            for (Quadrant quadrant : psc.getQuadrants()) {
+                int quadrantID = 1;
+                if (!firstRow) {
+                    insertUpdate.append(",\n");
+                }
+                insertUpdate.append("(" + searchID + ", (SELECT type_id from property_type WHERE type = '" + quadrant.name() + "'))");
+                firstRow = false;
+            }
+        }
+
     }
 
     UserTypeLogin attemptLogin(LoginInfo info) throws SQLException {
